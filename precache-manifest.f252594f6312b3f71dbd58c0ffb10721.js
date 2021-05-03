f26b3903c15b90e084369c37280ad48b5b048ebc
Bektur <nasirdinov080@gmail.com>
2021-05-03 21:37:54 +0600

    Updates

---
+++ precache-manifest.f252594f6312b3f71dbd58c0ffb10721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbe5d5bb857bdbcb66b040a04164406",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "e75c586317fa4050d43c",
+    "url": "/react-shop/static/css/main.2c86a493.chunk.css"
+  },
+  {
+    "revision": "3a06d945dbb59f7e249d",
+    "url": "/react-shop/static/js/2.507c8fb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-shop/static/js/2.507c8fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75c586317fa4050d43c",
+    "url": "/react-shop/static/js/main.fe23a90f.chunk.js"
+  },
+  {
+    "revision": "d4db231292616d992d22",
+    "url": "/react-shop/static/js/runtime-main.e598ee8f.js"
+  },
+  {
+    "revision": "118838167f52e918fc044b97cc42e081",
+    "url": "/react-shop/static/media/cart.11883816.svg"
+  }
+]);
